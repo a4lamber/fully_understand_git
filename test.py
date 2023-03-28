b3c267f27b4189449fb3f2c05776ed40cbccfedc
Adam Zhang <adamhimy19@gmail.com>
2023-03-27 22:06:20 -0400

    modified the addition function

---
+++ test.py
@@ -0,0 +1,26 @@
+'''
+ # @ Author: Your name
+ # @ Create Time: 2023-03-27 16:47:30
+ # @ Modified by: Your name
+ # @ Modified time: 2023-03-27 16:48:23
+ # @ Description: test scripts by prining 10
+ '''
+
+
+def addition(input_1,input_2):
+    """
+    return the addition of two numbers
+    Args:
+        input_1 (float): input 1 for addition
+        input_2 (float): input 2 for addition
+
+    Returns:
+        output (float): return the addition of two number.
+    """
+    output = input_1 + input_2
+    return output
+
+
+if __name__ == "__main__":
+    print(addition(3,4))
+
