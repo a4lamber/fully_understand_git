cbf4551892dc1bf17089f8a41fb1333476f3720e
Adam Zhang <adamhimy19@gmail.com>
2023-03-27 22:21:43 -0400

    Changed the function from addition to subtraction
    - since we needed to change get rid of addition
    - valla

--- test.py
+++ test.py
@@ -3,24 +3,24 @@
  # @ Create Time: 2023-03-27 16:47:30
  # @ Modified by: Your name
  # @ Modified time: 2023-03-27 16:48:23
- # @ Description: test scripts by prining 10
+ # @ Description: tested the script that takes substrction
  '''
 
 
-def addition(input_1,input_2):
+def minus(input_1,input_2):
     """
-    return the addition of two numbers
+    return the substraction of two numbers
     Args:
         input_1 (float): input 1 for addition
         input_2 (float): input 2 for addition
 
     Returns:
-        output (float): return the addition of two number.
+        output (float): return the subtraction of two number.
     """
-    output = input_1 + input_2
+    output = input_1 - input_2
     return output
 
 
 if __name__ == "__main__":
-    print(addition(3,4))
+    print(minus(3,4))
 
